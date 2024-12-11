1a74b9c95618a19b80bb54dff184c289b31060f1
101045330 <101045330@georgebrown.ca>
2024-12-11 11:14:56 -0500

    Updates

---
+++ precache-manifest.7e5e50fc453ff788b25e20ac9c3da949.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460df122b0baef88c92f4c1995f62686",
+    "url": "/a3desn3035-main/index.html"
+  },
+  {
+    "revision": "1ebededc1b221c5042dd",
+    "url": "/a3desn3035-main/static/css/2.be9925b5.chunk.css"
+  },
+  {
+    "revision": "046a5b539adfdf76b684",
+    "url": "/a3desn3035-main/static/css/main.c75e3022.chunk.css"
+  },
+  {
+    "revision": "1ebededc1b221c5042dd",
+    "url": "/a3desn3035-main/static/js/2.cb70e9c4.chunk.js"
+  },
+  {
+    "revision": "1876019e6f8cf7ca83e2",
+    "url": "/a3desn3035-main/static/js/3.0e4cdb98.chunk.js"
+  },
+  {
+    "revision": "046a5b539adfdf76b684",
+    "url": "/a3desn3035-main/static/js/main.42930b2b.chunk.js"
+  },
+  {
+    "revision": "312022a76020531c82dd",
+    "url": "/a3desn3035-main/static/js/runtime~main.045c5e99.js"
+  },
+  {
+    "revision": "25af2cd04ffdfc36c2bad76d16440ef9",
+    "url": "/a3desn3035-main/static/media/footer.25af2cd0.png"
+  }
+]);
